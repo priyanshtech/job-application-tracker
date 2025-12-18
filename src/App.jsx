53051c275cdbf9f42c434ec73ee736261745d29a
Priyansh Khatri <priyanshkhatri8690@gmail.com>
2025-12-18 15:49:13 +0530

    started new project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      Starter Ready
+    </div>
+  );
+}
